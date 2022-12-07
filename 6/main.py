995ab223996f19c69fedca1aa324aa75f862cf33
Derkness <tlpapae@gmail.com>
2022-12-07 19:47:05 +1030

    Remove redundant 'starting' variable
    This was used during a previous idea that was not pursued

--- 6/main.py
+++ 6/main.py
@@ -2,15 +2,10 @@ import collections
 
 def find_non_duplicate(limit):
      with open("input.txt", "r") as file:
-        starting = True
         line = file.readline()
         last4 = collections.deque(maxlen=limit)
         for index, char in enumerate(line):
             last4.append(char)
-            if (len(last4) == limit):
-                starting = False
-            if (starting):
-                continue
             if len(set(last4)) == limit:
                 return index+1
 
